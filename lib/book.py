6481989eeca0769218c87c9d3da9fae165d390c3
enjoroge <njrogeelizabeth53@gmail.com>
2024-07-09 23:54:10 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, page_count, author=None):
+        self.title = title
+        self.author = author
+        self.page_count = page_count  # Use the property setter for validation
     
-        
+    @property
+    def page_count(self):
+        """Get the number of pages in the book."""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self._condition = "Used"  # Initialize condition with a default value
+    
+    @property
+    def size(self):
+        """Get the size of the shoe."""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else: 
+            print('size must be an integer')
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self._condition = "New"
+
+    @property
+    def condition(self):
+        """Get the condition of the shoe."""
+        return self._condition
